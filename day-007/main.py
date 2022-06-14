67af4b57b3b437bc16d4715c8eff70339bf7bea3
Son Tu <thaisontunguyen@gmail.com>
2022-06-13 20:34:58 -0500

    Add ascii art

--- day-007/main.py
+++ day-007/main.py
@@ -1,19 +1,30 @@
 # hangman game
 from random_word import RandomWords
+from hangman_art import banner, hangman_stage
 from icecream import ic
 
+MAX_LIVES = 8
 
-def validate_input(current_guess) -> str:
+
+def validate_input(current_guess, history_guess) -> str:
     """Ask the user to input a character. Repeat if getting invalid input.
+       This also checks if the letter they guess is already guessed before,
+       as it is not a valid guess.
+
+    Args:
+        current_guess (str): the current word with correct characters revealed
+        history_guess (list[str]): list of historical guesses
 
     Returns:
         str: the valid character
     """
+
     valid_input = False
     while not valid_input:
         guess = str(input("Please enter a guess character: "))[0]
         if guess.lower().isalpha():
-            if guess.lower() not in current_guess:
+            if ((guess.lower() not in current_guess) and
+                    (guess.lower() not in history_guess)):
                 valid_input = True
             else:
                 print("Oops! You have already guessed this character! Try"
@@ -32,28 +43,34 @@ def hangman_word(word) -> bool:
     Returns:
         bool: True if guessing it correctly given 5 tries, False otherwise
     """
-    score = 0
+    stage = 0
     history_guess = []
     current_guess = ['_' for c in word]
-    while score < 5:
-        guess_char = validate_input(current_guess)
+    while stage < MAX_LIVES:
+        print('-------------------------------------------------')
+        guess_char = validate_input(current_guess, history_guess)
         isTrue, current_guess = check_and_show(word, current_guess, guess_char)
         if not isTrue:
-            score += 1
-            print("Oops! You guessed wrongly!")
+            stage += 1
+            print(f"Oops! You guessed wrongly! Letter {guess_char} is not"
+                  "in the word. You lose a life.")
         if current_guess == word:
-            print("You guessed correctly the word: You win!")
+            print(f"You guessed correctly the word. You win!" 
+                  "The word is == {word} ==!")
             return True
         history_guess.append(guess_char)
         print(f"The current word: {current_guess}")
         print(f"History of guess: {history_guess}")
-        print(f"The current score: {score}")
+        print(f"The current stage: {stage}")
+        print(hangman_stage[stage])
     print(f"Out of guesses! You lose! The correct word is {word}")
     return False
 
 
 def check_and_show(word, current_guess, guess_char) -> tuple[bool, str]:
     """Given a word and a character, check if the character is in the word.
+       Return True/False and also the form of the word with
+       revealed correct letters.
 
     Args:
         word (str): the word to guess
@@ -84,23 +101,18 @@ def check_and_show(word, current_guess, guess_char) -> tuple[bool, str]:
 
 def hangman_game() -> None:
     """The hanging man game with an internal or external database of words.
-       Using the Python library `random-word`
+       Using the Python library `random-word` for generating a word to guess.
     """
 
     random_word_instance = RandomWords()
-    word = random_word_instance.get_random_word()
+    long_word = random_word_instance.get_random_word()
+    word = long_word.split(" ")[0]
+    print(f"Phh! The word is {word}")
     hangman_word(word.lower())
 
 
 if __name__ == "__main__":
-    # hangman_word(word)
-    banner = """
-    ██╗  ██╗ █████╗ ███╗   ██╗ ██████╗       ███╗   ███╗ █████╗ ███╗   ██╗
-    ██║  ██║██╔══██╗████╗  ██║██╔════╝       ████╗ ████║██╔══██╗████╗  ██║
-    ███████║███████║██╔██╗ ██║██║  ███╗█████╗██╔████╔██║███████║██╔██╗ ██║
-    ██╔══██║██╔══██║██║╚██╗██║██║   ██║╚════╝██║╚██╔╝██║██╔══██║██║╚██╗██║
-    ██║  ██║██║  ██║██║ ╚████║╚██████╔╝      ██║ ╚═╝ ██║██║  ██║██║ ╚████║
-    ╚═╝  ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝ ╚═════╝       ╚═╝     ╚═╝╚═╝  ╚═╝╚═╝  ╚═══╝                                                                  
-    """
     print(banner)
     hangman_game()
+    # for i in range(0, MAX_LIVES):
+    #     print(hangman_stage[i])
