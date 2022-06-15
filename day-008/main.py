e06fab4aaa47db622bf7502c9155ea1c4872d00b
Son Tu <thaisontunguyen@gmail.com>
2022-06-14 23:36:03 -0500

    Add code for day 8

---
+++ day-008/main.py
@@ -0,0 +1,70 @@
+from utils import alphabet_list, banner, validate_direction
+from icecream import ic
+
+
+def encrypt_v2(text, shift) -> str:
+    """Take a text and shift every letter to `shift` characters
+    in the alphabet (using lambda function).
+
+    Args:
+        text (str): text to be encrypted
+        shift (int): number of characters to shift
+
+    Returns:
+        str: text after being shifted
+    """
+    shift_c = lambda c: alphabet_list[(alphabet_list.index(c) + shift) % len(alphabet_list)]
+    return_text = "".join([shift_c(c) for c in text])
+    return return_text
+
+
+def encrypt_v1(text, shift) -> str:
+    """Take a text and shift every letter to `shift` characters
+       in the alphabet.
+
+    Args:
+        text (str): text to be encrypted
+        shift (int): number of characters to shift
+
+    Returns:
+        str: text after being shifted
+    """
+    text_shifted = ""
+    for c in text:
+        idx = alphabet_list.index(c)
+        idx_shifted = (idx + shift) % len(alphabet_list)
+        text_shifted += alphabet_list[idx_shifted]
+    return(text_shifted)
+
+
+def decrypt(text, shift) -> str:
+    """Take a shifted text and un-shift every letter to
+    `shift` characters in the alphabet.
+
+    Args:
+        text (str): text to be decrypted
+        shift (int): number of characters to shift
+
+    Returns:
+        str: text after being un-shifted
+    """
+    return encrypt_v1(text, -shift)
+
+
+def CeasarCipher():
+    """The Ceasar Cipher program in command line
+    """
+    print(banner)
+    text = input("Please input the word: ")
+    shift = int(input("Please input the amount of shift: "))
+    direction = validate_direction()
+    if direction == 'e':
+        encrypt_text = encrypt_v1(text, shift)
+        print(f"The encrypted word is: {encrypt_text}")
+    else:
+        decrypt_text = decrypt(text, shift)
+        print(f"The decrypted word is: {decrypt_text}")
+
+
+if __name__ == "__main__":
+    CeasarCipher()
---
+++ day-008/utils.py
@@ -0,0 +1,41 @@
+# utilities for Ceasar-cipher
+import string
+from os import system, name
+from prettytable import PrettyTable
+
+alphabet_string = string.ascii_lowercase
+alphabet_list = list(alphabet_string)
+
+banner = """
+╔═╗┌─┐┌─┐┌─┐┌─┐┬─┐  ╔═╗┬ ┬┌─┐┬ ┬┌─┐┬─┐
+║  ├┤ ├─┤└─┐├─┤├┬┘  ║  └┬┘├─┘├─┤├┤ ├┬┘
+╚═╝└─┘┴ ┴└─┘┴ ┴┴└─  ╚═╝ ┴ ┴  ┴ ┴└─┘┴└─
+"""
+
+
+def clear():
+    """Clear the command promt
+    """
+    # for Window
+    if name == 'nt':
+        _ = system('cls')
+    # for Mac and Linux
+    else:
+        _ = system('clear')
+
+
+def validate_direction() -> str:
+    """Ask the user to input a direction. Repeat if getting invalid input.
+
+    Returns:
+        str: the valid direction
+    """
+    valid_input = False
+    while not valid_input:
+        direction = str(input("Please enter 'encrypt' (e)"
+                              "or 'decrypt' (d): ")[0])
+        if direction.lower() in ['e', 'd']:
+            valid_input = True
+        else:
+            print("Oops! That was not a valid choice! Try again...")
+    return direction
