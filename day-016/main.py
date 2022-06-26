550f72c367e60875f90089a8a86408d35fc42fd6
Son Tu <thaisontunguyen@gmail.com>
2022-06-26 00:42:26 -0500

    Update README

--- day-016/main.py
+++ day-016/main.py
@@ -1,31 +1,29 @@
-""" Script for testing out the turtle module
+""" Coffee Maker Object Oriented Programming
 """
-from time import sleep
-from turtle import Turtle, Screen
-import turtle
-from icecream import ic
+from menu import Menu, MenuItem
+from coffee_maker import CoffeeMaker
+from money_machine import MoneyMachine
 
-timmy = turtle.Turtle()
-timmy.dot(size=20)
-timmy.circle(200, 180)
-timmy.color("blue")
-g = timmy.stamp()
-timmy.backward(100)
-timmy.clearstamp(g)
-sleep(1)
+if __name__ == "__main__":
 
+    coffee_maker = CoffeeMaker()
+    money_machine = MoneyMachine()
+    menu = Menu()
 
-screen = timmy.getscreen()
-screen.bgcolor("orange")
-
-s = turtle.Shape("compound")
-print(s)
-poly1 = ((0, 0), (10, -5),(0,10),(-10,-5))
-s.addcomponent(poly1, "red", "blue")
-poly2 = ((0,0),(10,-5),(-10,-5))
-s.addcomponent(poly2, "blue", "red")
-print(s)
-turtle.register_shape("myshape", s)
-turtle.shape("myshape")
-
-sleep(5)
+    is_on = True
+    while is_on:
+        user_option = input(f"What would you like? {menu.get_items()}," +
+                            "or type 'report' for report,"
+                            "or 'off' to turn off the machine ")
+        if user_option == "off":
+            is_on = False
+        elif user_option == "report":
+            coffee_maker.report()
+            money_machine.report()  # I actually forgot this the first time
+        else:  # proceed with drink
+            order = menu.find_drink(order_name=user_option)
+            can_make = coffee_maker.is_resource_sufficient(order)
+            if can_make:
+                is_enough_money = money_machine.make_payment(order.cost)
+                if is_enough_money:
+                    coffee_maker.make_coffee(order=order)
