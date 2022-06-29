a0c98e0aa10a81927291b91fb53fec0e13eda7a6
Son Tu <thaisontunguyen@gmail.com>
2022-06-29 12:24:23 -0500

    Working code for day-017

--- day-017/data.py
+++ day-017/data.py
@@ -1,115 +1,26 @@
 """ Script to get new questions from Open Trivia API everytimne a new user plays
 """
-from unicodedata import name
 import requests
 import json
+import re
+from icecream import ic 
 
 
-question_data = [
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The HTML5 standard was published in 2014.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "The first computer bug was formed by faulty wires.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "FLAC stands for 'Free Lossless Audio Condenser'.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "All program codes have to be compiled into an executable file in order to be run. This file can then be executed on any machine.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "Linus Torvalds created Linux and Git.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "The programming language 'Python' is based off a modified version of 'JavaScript'",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "medium",
-        "question": "AMD created the first consumer 64-bit processor.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "'HTML' stands for Hypertext Markup Language.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "easy",
-        "question": "In most programming languages, the operator ++ is equivalent to the statement '+= 1'.",
-        "correct_answer": "True",
-        "incorrect_answers": [
-            "False"
-        ]
-    },
-    {
-        "category": "Science: Computers",
-        "type": "boolean",
-        "difficulty": "hard",
-        "question": "The IBM PC used an Intel 8008 microprocessor clocked at 4.77 MHz and 8 kilobytes of memory.",
-        "correct_answer": "False",
-        "incorrect_answers": [
-            "True"
-        ]
-    }
-]
+def get_questions_bank(url_API='https://opentdb.com/api.php?amount=10&type=boolean', amount=10, type='boolean') -> list:
+    """ Get json data from the API, use `re` to change the amount in the API url
+
+    Args:
+        url_API (str): API url
+        amount (int): number of questions, default to 10
+        type (str): type of questions, dafault to 'boolean'
+
+    Returns:
+        list: data in format list[dict]
+    """
+    url_API = re.sub("[1-9][0-9]", str(amount), url_API, 1)
+    response_API = requests.get(url_API)
+    return response_API.json()['results']
 
 
 if __name__ == "__main__":
-    response_API = requests.get('https://opentdb.com/api.php?amount=10&type=boolean')
-    print(response_API.status_code)
-    print(response_API.json())
+    print(get_questions_bank(amount=2))
--- day-017/main.py
+++ day-017/main.py
@@ -1,6 +1,6 @@
 """  The Script for the game
 """
-from data import question_data
+import data
 NUM_QUESTIONS = 10
 
 
@@ -10,10 +10,13 @@ class Question:
         self.answer = answer
 
 
-questions_bank = []
-for item in question_data:
-    question = Question(item["question"], item["correct_answer"])
-    questions_bank.append(question)
+def make_question(amount=10):
+    question_data = data.get_questions_bank(amount=amount)
+    questions_bank = []
+    for item in question_data:
+        question = Question(item["question"], item["correct_answer"])
+        questions_bank.append(question)
+    return questions_bank
 
 
 class QuizBrain:
@@ -26,7 +29,7 @@ class QuizBrain:
         question = self.questions_list[self.question_number]
         user_input = 'None'
         while user_input.lower()[0] not in ['t', 'f']:
-            user_input = input(f"Q{self.question_number}. {question.text} "
+            user_input = input(f"Q{self.question_number + 1}. {question.text} "
                                "True or False? ")
         if user_input == 't':
             user_choice = "True"
@@ -39,14 +42,15 @@ class QuizBrain:
         else:
             print(f"You are incorrect, your score is "
                   f"{quizbrain.score}/{self.question_number + 1}.")
+
         print(f"Q{self.question_number + 1}. {question.text} "
               f"{question.answer}. ")
         self.question_number += 1
 
 
 if __name__ == "__main__":
+    questions_bank = make_question(amount=NUM_QUESTIONS)
     quizbrain = QuizBrain(questions_bank)
-    print('f')
+
     while quizbrain.question_number < len(quizbrain.questions_list):
         quizbrain.next_question()
-
